85a07d9228532fbe3507f22f55277ff8618ee341
Jonathan Lin <jontitan@gmail.com>
2020-10-11 21:21:51 -0500

    Add first sets of test and demo

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 public class Demo {
 
+    public List<String> upperCase(List<String> input) {
+        List<String> output = new ArrayList<>();
+        for (String s : input) {
+            output.add(s.toUpperCase());
+        }
+        return output;
+    }
 }
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DemoTest {
+
+    Demo subject = new Demo();
+
     @Test
-    void name() {
-        assertTrue(false);
+    void upperCase() {
+        List<String> input = List.of("ABC", "abc", "ABc", "aBC");
+
+        List<String> output = subject.upperCase(input);
+
+        assertThat(output).containsOnly("ABC");
     }
 }
